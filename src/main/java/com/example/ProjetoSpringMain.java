77ab6c1e025a3a3033d60bbe6f1244e78edf52db
Renan Dias <129848338+RenanGDias@users.noreply.github.com>
2025-04-09 17:21:45 -0300

    Aula 2: Configuração do ambiente de desenvolvimento

---
+++ src/main/java/com/example/ProjetoSpringMain.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringMain.class, args);
+		System.out.println("Olá mundo!");
+	}
+
+}
